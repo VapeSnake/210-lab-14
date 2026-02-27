9e65ca10ba3f2c2eec463827d34e759e0eabec5b
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-26 16:16:29 -0800

    3rd Commit: Class members and methods complete.

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -13,7 +13,7 @@ class Color {
     public:
     int getRed() const { return red; }
     void setRed(int r) { red = r; }
-    int getGreen() const { return green; } // Getter functions const to avoid modifying objects.
+    int getGreen() const { return green; } // Getter functions const to avoid modifying objects. Used by print() to access private member variables.
     void setGreen(int g) { green = g; }   // Setter functions to modify private member variables.
     int getBlue() const { return blue; }
     void setBlue(int b) { blue = b; }
@@ -21,12 +21,19 @@ class Color {
     void print() const {
         cout << setw(TABLE_WIDTH) << "R: " << getRed() << " "
              << setw(TABLE_WIDTH) << "G: " << getGreen() << " "
-             << setw
-
+             << setw(TABLE_WIDTH) << "B: " << getBlue() << endl; // Prints color's RGB values.
     }
-}
+};
 
 int main() {
+    Color Yellow; //RGB values are 255, 224, 32.
+    Yellow.setRed(255);
+    Yellow.setGreen(224); // Setting RGB values for Yellow color using setter functions.
+    Yellow.setBlue(32);  // Useful for controlled access to private member variables.
+    cout << "Yellow's RGB Values are:" << endl;
+    Yellow.print();
+
+    
 
 
 }
